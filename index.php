58d6b9b135b330435134e0afc591d9a442557b23
saokizayta <56785237+saokizayta@users.noreply.github.com>
2021-10-06 20:54:02 +0700

    add file

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
                         <div class="row">
                             <div class="col-lg-8 col-md-8 col-sm-8">
                                 <div class="section-title">
-                                    <h4>TIN TỨC</h4>
+                                    <h4>THÔNG BÁO</h4>
                                 </div>
                             </div>
                             <div class="col-lg-4 col-md-4 col-sm-4">
@@ -64,13 +64,13 @@
                             <div class="col-lg-4 col-md-6 col-sm-6">
                                 <div class="product__item">
                                     <div class="product__item__pic set-bg" data-setbg="img/trending/trend-1.jpg">
-                                        <div class="ep">18 / 18</div>
-                                        <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                                        <div class="ep">2 min ago</div>
+                                        <div class="comment"><i class="fa fa-clock"></i> 11</div>
                                         <div class="view"><i class="fa fa-eye"></i> 9141</div>
                                     </div>
                                     <div class="product__item__text">
                                         <ul>
-                                            <li>Active</li>
+                                            <li>Active</li><br>
                                             <li>Movie</li>
                                         </ul>
                                         <h5><a href="#">The Seven Deadly Sins: Wrath of the Gods</a></h5>
