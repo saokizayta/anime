c2fea7a52ea20e8d378090af3376f6805b686d38
saokizayta <56785237+saokizayta@users.noreply.github.com>
2021-10-07 19:34:16 +0700

    Commit

---
+++ character.php
@@ -0,0 +1,55 @@
+<link rel="stylesheet" href="https://pristontale.eu/css/characters.css" type="text/css">
+<div class="explore-others-container">
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='fighter.php'">
+            <div class="char-explore-link-text">Fighter</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/fighter.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='archer.php'">
+            <div class="char-explore-link-text">Archer</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/archer.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='mechanician.php'">
+            <div class="char-explore-link-text">Mechanician</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/mechanician.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='assassin.php'">
+            <div class="char-explore-link-text">assassin</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/assassin.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='pikeman.php'">
+            <div class="char-explore-link-text">Pikeman</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/pikeman.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='knight.php'">
+            <div class="char-explore-link-text">Knight</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/knight.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='shaman.php'">
+            <div class="char-explore-link-text">Shaman</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/shaman.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='priestess.php'">
+            <div class="char-explore-link-text">Priestess</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/priestess.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='magician.php'">
+            <div class="char-explore-link-text">Magician</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/magician.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+        <div class="char-explore-link-container" onclick="javascript:location.href='atalanta.php'">
+            <div class="char-explore-link-text">atalanta</div>
+            <div class="explore-link-img-holder" style="background: url(img/character_explore_links/atalanta.png); background-position: center; background-size: cover; background-repeat: no-repeat;"></div>
+        </div>
+
+    </div>
