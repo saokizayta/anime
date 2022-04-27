c5137bc36d8877298c324291c8b7b00696b1d2e3
saokizayta <56785237+saokizayta@users.noreply.github.com>
2022-04-27 11:50:49 +0700

    Update admin folder

---
+++ admin/@gameserver/config.php
@@ -0,0 +1,12 @@
+<?php
+//$hostname = "sql202.freevnn.com";
+//$dbname = "freev_15676079_company";
+//$username = "freev_15676079";
+//$pw = "theanh";
+
+$hostname = "localhost";
+$dbname = "ptvui_2022";
+$username = "root";
+$pw = "";
+
+?>
