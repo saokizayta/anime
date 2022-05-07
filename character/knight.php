666ec1ea10616d9b160e481a80137dcd5672f9f7
Your Name <you@example.com>
2022-05-07 22:22:06 +0700

    Ngủ

---
+++ character/knight.php
@@ -0,0 +1,18 @@
+<div class="header-character header-knight">
+        <div class="char-page-navigation">
+            <!-- <div id="overview">Overview</div> <div class="nav-spacer" style="letter-spacing: -2px;">--</div>
+            <div id="mechanics">Mechanics</div> <div class="nav-spacer" style="letter-spacing: -2px;">--</div>
+            <div id="skills">Skills</div> <div class="nav-spacer" style="letter-spacing: -2px;">--</div>
+            <div id="our-chars">Our Heroes</div> -->
+        </div>
+
+        <div class="overview-section">
+            <!-- <div class="hero-list"><a href="characters.php">Hero List</a></div> -->
+            <div class="char-title">Knight</div>
+            <div class="char-sub-title">The Paladin of All Trades</div>
+            <div class="char-description">
+                Empowered by the gods, the knight can attack, tank, and even support his allies all at once. He was once a Tempskron warrior but Morion magic made him too powerful to ever come back.
+            </div>
+
+         </div>
+</div>
